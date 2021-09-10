877c0240af52ad34f49c3b8d8f1c82713e5002b4
Jayjay222 <88888683+Jayjay222@users.noreply.github.com>
2021-09-10 20:54:24 +0700

    Add files via upload

---
+++ bai1A.js
@@ -0,0 +1,13 @@
+var DaiNhat= '';
+
+var plorp = ["Long", "khanh", "ha", "an", "khai", "tri", "fjhvhhjjjjj", "dvvvwz"];
+
+function longestString(arr){
+  for (i=0; i<arr.length; i++){
+      if (arr[i].length > DaiNhat.length){
+        DaiNhat= arr[i].length;
+      }
+
+  }
+  return DaiNhat;
+}
---
+++ baiB.js
@@ -0,0 +1,31 @@
+body{
+    margin:0;
+    padding: 0;
+    background: #e74c3c;
+    font-family: "Calibri";
+    }
+    .color{
+        margin-top: 300px;
+        text-align: center;
+
+    }
+
+    #hex{
+        display: block;
+        color: white;
+        font-size: 40px;
+    
+    }
+    .color button{
+        background: none;
+        outline: none;
+        color: white;
+        border: 3px soild white;
+        cursor: Pointer;
+        font-size: 24px;
+        
+    }
+
+
+
+
